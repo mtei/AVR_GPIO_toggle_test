4c200c9ba5e9721d7d7c77517d4e4654c1195e20
mtei <2170248+mtei@users.noreply.github.com>
2020-04-13 20:07:38 +0900

    fix typo

--- toggle_uno.c
+++ toggle_uno.c
@@ -25,32 +25,32 @@ void run(void)
     _delay_ms(250);
 }
 
-void bit3_toggle_1(void)
+void bit5_toggle_1(void)
 {
     PIND |= (1<<5);
     // with -O1  OK
-    //    000000ee <bit3_toggle_1>:
+    //    000000ee <bit5_toggle_1>:
     //      ee:   4d 9a           sbi     0x09, 5 ; 9
     //      f0:   08 95           ret
     // with -O0  NG
 }
 
-void bit3_toggle_2(void)
+void bit5_toggle_2(void)
 {
     PIND = (1<<5);
     // with -O1  OK
-    //    000000f2 <bit3_toggle_2>:
+    //    000000f2 <bit5_toggle_2>:
     //      f2:   80 e2           ldi     r24, 0x20       ; 32
     //      f4:   89 b9           out     0x09, r24       ; 9
     //      f6:   08 95           ret
     // with -O0  OK
 }
 
-void bit3_toggle_3(void)
+void bit5_toggle_3(void)
 {
     PORTD ^= (1<<5);
     // with -O1  OK
-    //    000000f8 <bit3_toggle_3>:
+    //    000000f8 <bit5_toggle_3>:
     //      f8:   8b b1           in      r24, 0x0b       ; 11
     //      fa:   90 e2           ldi     r25, 0x20       ; 32
     //      fc:   89 27           eor     r24, r25
@@ -65,18 +65,18 @@ void bit3_toggle_3(void)
 //   | PIND  = (1<<5)   |    OK       |     OK       |    2 step                 |
 //   | PORTD ^= (1<<5)  |    OK       |     OK       |    4 step                 |
 
-void bit3_toggle_test(void)
+void bit5_toggle_test(void)
 {
     for( uint8_t i = 0; i < 8; i++ ) {
         _delay_ms(500);
 #ifdef TEST_1
-        bit3_toggle_1();
+        bit5_toggle_1();
 #endif
 #ifdef TEST_2
-        bit3_toggle_2();
+        bit5_toggle_2();
 #endif
 #ifdef TEST_3
-        bit3_toggle_3();
+        bit5_toggle_3();
 #endif
     }
     _delay_ms(500);
@@ -88,9 +88,9 @@ int main(void)
     while(1) {
         run();
         PORTD = 0xFC;
-        bit3_toggle_test();
+        bit5_toggle_test();
         PORTD = 0x84;
-        bit3_toggle_test();
+        bit5_toggle_test();
     }
 }
 
